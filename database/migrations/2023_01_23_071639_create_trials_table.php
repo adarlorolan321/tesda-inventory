07bc598357c0696847e5018c4836267d3eaa4d21
Kobe Ferran <emailme.kobeferran@gmail.com>
2023-01-23 15:46:06 +0800

    service api unit test

--- database/migrations/2023_01_23_071639_create_trials_table.php
+++ database/migrations/2023_01_23_071639_create_trials_table.php
@@ -13,16 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('trials', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('class_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('student_id')->nullable()->constrained()->cascadeOnDelete();
-            $table->foreignId('organisation_id')->constrained()->cascadeOnDelete();
-            $table->string('status');
-            // $table-> lesson
-            $table->timestamps();
-        });
+        // Schema::create('trials', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->foreignId('class_id')->constrained()->cascadeOnDelete();
+        //     $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+        //     $table->foreignId('student_id')->nullable()->constrained()->cascadeOnDelete();
+        //     $table->foreignId('organisation_id')->constrained()->cascadeOnDelete();
+        //     $table->string('status');
+        //     // $table-> lesson
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('trials');
+        // Schema::dropIfExists('trials');
     }
 };
