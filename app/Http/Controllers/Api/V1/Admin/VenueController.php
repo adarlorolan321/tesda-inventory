01a0dbb36200c1f44bdc292e71028f2d1f10c082
Kobe Ferran <emailme.kobeferran@gmail.com>
2023-02-06 13:55:49 +0800

    venue get list resource update

--- app/Http/Controllers/Api/V1/Admin/VenueController.php
+++ app/Http/Controllers/Api/V1/Admin/VenueController.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreVenueRequest;
 use App\Http\Requests\Admin\UpdateVenueRequest;
 use App\Http\Resources\VenueResource;
+use App\Models\Organisation;
 use App\Models\Venue;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class VenueController extends Controller
@@ -20,29 +22,53 @@ class VenueController extends Controller
      */
     public function index(Request $request)
     {
-        \abort_if(!\auth()->user()->can('access venue'), Response::HTTP_FORBIDDEN, 'Unauthorized');
+        $user = Auth::user();
+
+        \abort_if(!$user->can('access venue'), Response::HTTP_FORBIDDEN, 'Unauthorized');
 
         $perPage = $request->has('perPage') ? $request->input('perPage') : 10;
 
-        $venues = Venue::with(['organisation'])
-            ->where(function ($query) use ($request) {
-                if ($request->has('query')) {
-                    $s = $request->input('query');
+        $venueTableName = app(Venue::class)->getTable();
+        $organisationTableName = app(Organisation::class)->getTable();
 
-                    $query->where('name', 'like', '%' . $s . '%')
-                        ->orWhere('contact_first_name', 'like', '%' . $s . '%')
-                        ->orWhere('contact_last_name', 'like', '%' . $s . '%')
-                        ->orWhere('contact_email', 'like', '%' . $s . '%')
-                        ->orWhere('contact_phone', 'like', '%' . $s . '%')
-                        ->orWhere(DB::raw("CASE WHEN status = 1 THEN 'active' WHEN status = 0 THEN 'inactive' END"), 'like', '%' . $s . '%')
-                        ->orWhereHas('organisation', function ($query) use ($s) {
-                            $query->where('name', 'like', '%' . $s . '%');
-                        });
-                }
+        return DB::table('venues')
+            ->where(function ($query) use ($request, $venueTableName, $organisationTableName) {
+                $s = $request->input('query');
+                $query->when($request->has('query'), function ($query) use (
+                    $s,
+                    $venueTableName,
+                    $organisationTableName
+                ) {
+                    $query->where($venueTableName . '.name', 'like', '%' . $s . '%')
+                        ->orWhere($venueTableName . '.contact_first_name', 'like', '%' . $s . '%')
+                        ->orWhere($venueTableName . '.contact_last_name', 'like', '%' . $s . '%')
+                        ->orWhere($venueTableName . '.contact_email', 'like', '%' . $s . '%')
+                        ->orWhere($venueTableName . '.contact_phone', 'like', '%' . $s . '%')
+                        ->orWhere(
+                            DB::raw("CASE WHEN " .
+                                $venueTableName . ".status = 1 THEN 'active' WHEN " .
+                                $venueTableName . ".status = 0 THEN 'inactive' END"),
+                            'like',
+                            '%' . $s . '%'
+                        )
+                        ->orWhere('organisation.name', 'like', '%' . $s . '%');
+                });
             })
+            ->when($user->hasRole('orgadmin'), function ($query) use ($user) {
+                $query->where('organisation_id', $user->organisation_id);
+            })
+            ->leftJoin(
+                $organisationTableName . ' as organisation',
+                $venueTableName . '.organisation_id',
+                '=',
+                'organisation.id',
+            )
+            ->select(
+                $venueTableName . '.*',
+                'organisation.name as organisation'
+            )
+            ->orderBy($venueTableName . '.name', 'ASC')
             ->paginate($perPage);
-
-        return VenueResource::collection($venues);
     }
 
     /**
