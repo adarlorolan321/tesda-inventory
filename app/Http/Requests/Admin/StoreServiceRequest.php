a77293cc29c4187b32ed1e7a53601acd5f42b1f7
Kobe Ferran <emailme.kobeferran@gmail.com>
2023-02-09 12:51:31 +0800

    make code nullable in services

--- app/Http/Requests/Admin/StoreServiceRequest.php
+++ app/Http/Requests/Admin/StoreServiceRequest.php
@@ -48,6 +48,7 @@ class StoreServiceRequest extends FormRequest
                 })
             ],
             'code' => [
+                'nullable',
                 'string',
                 'unique:services,code'
             ],
