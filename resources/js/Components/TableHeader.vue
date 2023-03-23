37e628d5b8b3111fc213506fcc49348a1fb67bca
Mark Alfred M. Macapagal <mark.alfred072915@gmail.com>
2023-03-23 14:01:08 +0800

    Add theader

---
+++ resources/js/Components/TableHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <th
+        class="sortable"
+    >
+    
+        <slot></slot>
+        <i
+        class="ti ti-arrow-up"
+        v-if="sortUp"
+        ></i>
+        <i
+        class="ti ti-arrow-down"
+        v-if="sortDown"
+        ></i>
+    </th>
+</template>
+<script>
+    export default {
+        props: ["serverQuery", "serverQueryKey"],
+        computed: {
+            sortUp: function(){
+                return this.serverQuery.sort == this.serverQueryKey && this.serverQuery.order == 'desc'
+            },
+            sortDown: function(){
+                return this.serverQuery.sort == this.serverQueryKey && this.serverQuery.order == 'asc'
+            }
+        }
+    }
+</script>
