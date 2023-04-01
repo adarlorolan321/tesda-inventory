6acf3784ae195df73e690b98f868d88299609677
adarlorolan321 <adarlorolan14@gmail.com>
2023-04-01 20:19:11 +0800

    assetUrl

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,27 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [
-    
-    laravel({
-      input: 'resources/js/app.js',
-      refresh: true,
-      secure: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
-      },
-    }),
+    vue(),
   ],
   server: {
-    https: true, // enable HTTPS
+    https: true,
   },
   build: {
     manifest: true,
+    assetsDir: 'assets',
     rollupOptions: {
       input: 'resources/js/app.js',
+      output: {
+        dir: 'public/build/assets',
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]',
+        // Use absolute URLs for assets to ensure they are loaded over HTTPS
+        // Replace "https://yourdomain.com" with your actual domain
+        assetUrls: (asset) => `https://tesda-inventory.herokuapp.com/build/assets/${asset.name}${asset.ext}`,
+      },
     },
   },
 });
