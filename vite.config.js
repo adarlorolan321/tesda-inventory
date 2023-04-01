428211e3815e2574c237d620e858f765bcf80ec8
adarlorolan321 <adarlorolan14@gmail.com>
2023-04-01 14:17:03 +0800

    Commit message describing your changes

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,27 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  server: {
+    https: true, // enable HTTPS
+  },
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: 'resources/js/app.js',
+    },
+  },
 });
